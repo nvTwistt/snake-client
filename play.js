37998a091719eed12af29429b2b3f880b74c4813
nv_twistt <mtamkee@icloud.com>
2021-06-01 14:53:56 -0600

    added control

--- play.js
+++ play.js
@@ -1,17 +1,6 @@
-const conn = require("./client");
-conn();
+const connect = require("./client");
+const setupInput = require('./input');
+connect();
 console.log("Connecting ...");
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+
 setupInput();
